fd01cfc51717f01ea0664b19bb68d7a70ed8f81c
bemyhre <ben.myhre35@gmail.com>
2014-06-07 15:17:02 -0700

    hello world - added new route/table

--- craigslist-job-search/db/schema.rb
+++ craigslist-job-search/db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 1) do
+ActiveRecord::Schema.define(:version => 2) do
 
   create_table "listings", :force => true do |t|
     t.string   "listing_id"
     t.string   "title"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "searches", :force => true do |t|
+    t.string   "search_name"
+    t.string   "string"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
   end
 
 end
