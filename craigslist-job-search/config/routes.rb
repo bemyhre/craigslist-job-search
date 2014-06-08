e3a347c1c9a1df0f366aea2c14ed60ca78af6c55
bemyhre <ben.myhre35@gmail.com>
2014-06-07 17:09:34 -0700

    display front page

--- craigslist-job-search/config/routes.rb
+++ craigslist-job-search/config/routes.rb
@@ -1,4 +1,7 @@
 CraigslistJobSearch::Application.routes.draw do
+  root :to => 'listings#index'
+  resources :searches
+  resources :listings
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- craigslist-job-search/db/schema.rb
+++ craigslist-job-search/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 2) do
+ActiveRecord::Schema.define(:version => 3) do
+
+  create_table "listings", :force => true do |t|
+    t.string   "listing_id"
+    t.string   "title"
+    t.integer  "search_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "searches", :force => true do |t|
     t.string   "search_name"
